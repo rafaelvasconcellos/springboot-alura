1a09847134d1e4ae949d1128402afb95158e79a2
Rafael Vasconcellos <rafael.vasconcellos@accenture.com>
2019-04-29 10:47:43 -0300

    Adicionando o Thymeleaf e o Bootstrap

--- src/main/java/br/com/alura/listaVip/Configuracao.java
+++ src/main/java/br/com/alura/listaVip/Configuracao.java
@@ -3,18 +3,10 @@ package br.com.alura.listaVip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
 public class Configuracao {
-
-    @RequestMapping("/")
-    @ResponseBody
-    public String Ola(){
-        return "Bão meus fi";
-    }
 
 
     public static void main(String[] args) {
---
+++ src/main/java/br/com/alura/listaVip/ConvidadoController.java
@@ -0,0 +1,13 @@
+package br.com.alura.listaVip;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ConvidadoController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
