b237251b8e59a9ef9a5ce5c98cc9e546e068a584
Rafael Vasconcellos <rafael.vasconcellos@accenture.com>
2019-05-02 11:31:57 -0300

    Adicionando a estratégia de geração de id

--- src/main/java/br/com/alura/listaVip/model/Convidado.java
+++ src/main/java/br/com/alura/listaVip/model/Convidado.java
@@ -2,18 +2,27 @@ package br.com.alura.listaVip.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name="convidado")
 public class Convidado {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
     private String email;
     private String telefone;
+
+    public Convidado(){}
+
+    public Convidado(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
 
     public Long getId() {
         return id;
