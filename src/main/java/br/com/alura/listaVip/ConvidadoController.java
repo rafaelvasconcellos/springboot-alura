b1d3b9177dcf527b41547a3ca4343ffcdb649464
Rafael Vasconcellos <rafael.vasconcellos@accenture.com>
2019-05-02 11:31:27 -0300

    Salvando um novo convidado

--- src/main/java/br/com/alura/listaVip/ConvidadoController.java
+++ src/main/java/br/com/alura/listaVip/ConvidadoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ConvidadoController {
@@ -21,6 +23,19 @@ public class ConvidadoController {
     @RequestMapping("convidados")
     public String listaConvidados(Model model)
     {
+        Iterable<Convidado> convidados = repository.findAll();
+        model.addAttribute("convidados", convidados);
+
+        return "listaConvidados";
+    }
+
+    @RequestMapping(value= "salvar", method = RequestMethod.POST)
+    public String salvar(@RequestParam("nome") String nome, @RequestParam("email") String email,
+                         @RequestParam("telefone") String telefone, Model model ){
+
+        Convidado novoConvidado = new Convidado(nome, email, telefone);
+        repository.save(novoConvidado);
+
         Iterable<Convidado> convidados = repository.findAll();
         model.addAttribute("convidados", convidados);
 
